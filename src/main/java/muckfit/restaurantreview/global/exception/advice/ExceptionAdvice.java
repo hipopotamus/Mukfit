8b649f7396d05f1b8ebb35d4e90c592c3a628613
hipopotamus <qkqntjddn@gmail.com>
2025-09-02 14:05:39 +0900

    [Feat] ExceptionAdvice 구현 컨트롤러를 통해 발생하는 에러를 처리하기 위해 ExceptionAdvice를 구현하였습니다. ExceptionAdvice는 다음과 같은 에러를 후처리합니다. 1. 일반적으로 발생하는 에러 2. 비지니스 로직에서 발생하는 에러 3. 클라이언트의 입력에서 발생하는 bind 에러

---
+++ src/main/java/muckfit/restaurantreview/global/exception/advice/ExceptionAdvice.java
@@ -0,0 +1,45 @@
+package muckfit.restaurantreview.global.exception.advice;
+
+import muckfit.restaurantreview.global.exception.BusinessLogicException;
+import muckfit.restaurantreview.global.exception.ExceptionCode;
+import muckfit.restaurantreview.global.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Component
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> basicExceptionHandler(Exception e) {
+
+        ErrorResponse errorResponse =
+                new ErrorResponse(e.getClass().getSimpleName(), e.getMessage(),
+                        ExceptionCode.GLOBAL_EXCEPTION.getCode());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> businessLoginExceptionHandler(BusinessLogicException e) {
+        ExceptionCode exceptionCode = e.getExceptionCode();
+        ErrorResponse errorResponse =
+                new ErrorResponse(e.getClass().getSimpleName(), e.getMessage(), exceptionCode.getCode());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(exceptionCode.getStatus()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> bindExceptionHandler(BindException e) {
+
+        ExceptionCode bindException = ExceptionCode.BIND_EXCEPTION;
+        ErrorResponse errorResponse =
+                new ErrorResponse(e.getClass().getSimpleName(), bindException.getMessage(), bindException.getCode());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}

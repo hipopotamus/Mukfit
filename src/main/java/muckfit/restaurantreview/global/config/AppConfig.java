09bc7dbad8a2fa4df86a487217e575d4d19596be
hipopotamus <qkqntjddn@gmail.com>
2025-09-15 16:13:38 +0900

    [Feat] P6spy 구현 JPA로 발생하는 query를 확인 할 수 있도록 P6spy를 구현하였습니다.

--- src/main/java/muckfit/restaurantreview/global/config/AppConfig.java
+++ src/main/java/muckfit/restaurantreview/global/config/AppConfig.java
@@ -1,8 +1,11 @@
 package muckfit.restaurantreview.global.config;
 
+import com.p6spy.engine.spy.P6SpyOptions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import muckfit.restaurantreview.global.p6spy.P6spySqlFormatConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class AppConfig {
 
-
     private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
+    }
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());
     }
 }

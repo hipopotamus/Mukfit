b2fa78bdf52e312bb4452a3a992f294c235821c1
hipopotamus <qkqntjddn@gmail.com>
2025-09-15 16:25:01 +0900

    [Fix] P6spy 수정 1. P6spySqlFormatConfiguration에 @Component를 붙여 자동으로 빈에 등록되도록 조치하였습니다. 때문에 AppConfig에 정의 했던 setLogMessageFormat을 제거하였습니다. 2. P6spySqlFormatConfiguration에 메서드 구문 중 비효율적인 부분을 수정하였습니다.

--- src/main/java/muckfit/restaurantreview/global/config/AppConfig.java
+++ src/main/java/muckfit/restaurantreview/global/config/AppConfig.java
@@ -1,11 +1,8 @@
 package muckfit.restaurantreview.global.config;
 
-import com.p6spy.engine.spy.P6SpyOptions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import muckfit.restaurantreview.global.p6spy.P6spySqlFormatConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,10 +15,5 @@ public class AppConfig {
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
-    }
-
-    @PostConstruct
-    public void setLogMessageFormat() {
-        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());
     }
 }
--- src/main/java/muckfit/restaurantreview/global/p6spy/P6spySqlFormatConfiguration.java
+++ src/main/java/muckfit/restaurantreview/global/p6spy/P6spySqlFormatConfiguration.java
@@ -3,26 +3,29 @@ package muckfit.restaurantreview.global.p6spy;
 import com.p6spy.engine.logging.Category;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 import org.hibernate.engine.jdbc.internal.FormatStyle;
+import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+@Component
 public class P6spySqlFormatConfiguration implements MessageFormattingStrategy {
 
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
 
         sql = formatSql(category, sql);
-        return "|P6Spy sql|" + category + "|" + sql;
+        return String.format("|P6Spy sql|%s|%s", category, sql);
     }
 
-    private String formatSql(String category,String sql) {
+    private String formatSql(String category, String sql) {
 
-        if(sql == null || sql.trim().equals("")) return sql;
+        if (sql == null || sql.isBlank()) return sql;
 
         // Only format Statement, distinguish DDL And DML
         if (Category.STATEMENT.getName().equals(category)) {
             String tmpsql = sql.trim().toLowerCase(Locale.ROOT);
-            if(tmpsql.startsWith("create") || tmpsql.startsWith("alter") || tmpsql.startsWith("comment")) {
+            if(tmpsql.startsWith("create") || tmpsql.startsWith("alter") || tmpsql.startsWith("comment")
+                    || tmpsql.startsWith("drop") || tmpsql.startsWith("truncate")) {
                 sql = FormatStyle.DDL.getFormatter().format(sql);
             }else {
                 sql = FormatStyle.BASIC.getFormatter().format(sql);
